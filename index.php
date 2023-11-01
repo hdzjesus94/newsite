952f3547ae26aea7ffdcfe1505d56cc4597c373d
hdzjesus94 <87738989+hdzjesus94@users.noreply.github.com>
2023-10-31 18:06:02 -0600

    Rolling-back-PHP-proyect

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php 
+    $title= 'Index';
+    require_once 'include/header.php'
+?>
+<!-- 
+    -First Name
+    -Last Name
+    -Date of Birth (Use Date Picker)
+    -Speciality (Database admin, Software Developer, Web Administrator, Other)
+    -Email Addres
+    -Contact Number
+-->
+    <h1 class="text-center">Registration for IT Conference</h1>
+    <form method="get" action="success.php">
+        <div class="mb-3">
+            <label for="firstname" class="form-label">First Name</label>
+            <input type="text" class="form-control" id="firstname">
+        </div>
+        <div class="mb-3">
+            <label for="lastname" class="form-label">Last Name</label>
+            <input type="text" class="form-control" id="lastname">
+        </div>
+        <div class="mb-3">
+            <label for="dob" class="form-label">Date Of Birth</label>
+            <input type="text" class="form-control" id="dob">
+        </div>
+        <div class="mb-3">
+            <label for="specialty">Area of Expertise</label>
+            <select class="form-select" id="specialty">
+                <option selected>Database Admin</option>
+                <option value="1">Software Developer</option>
+                <option value="2">Web Administrator</option>
+                <option value="3">Other</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter a email" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else</small>
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Contact Number</label>
+            <input type="text" class="form-control" id="phone" aria-describedby="phoneHelp">
+            <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else</small>
+        </div>
+   
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+<?php require_once 'include/footer.php'?>
---
+++ success.php
@@ -0,0 +1,12 @@
+<?php 
+    $title= 'Index';
+    require_once 'include/header.php'
+?>
+<h1 class="text-center text-success">You have been registered!</h1>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<?php require_once 'include/footer.php'?>
